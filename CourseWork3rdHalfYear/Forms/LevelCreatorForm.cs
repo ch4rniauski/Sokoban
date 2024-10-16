5ad22705dbd2c484d2269fd96c68e702a41f1e62
Yauheni Charniauski <chernyavskij.z@mail.ru>
2024-10-16 19:14:33 +0300

    Compact code(remove useless & add List<Controls>)

--- CourseWork3rdHalfYear/Forms/LevelCreatorForm.cs
+++ CourseWork3rdHalfYear/Forms/LevelCreatorForm.cs
@@ -1,33 +1,13 @@
-﻿using Microsoft.VisualBasic.Devices;
-using System.Data;
-using System.Windows.Forms;
+﻿using System.Data;
 
 namespace CourseWork3rdHalfYear.Forms
 {
     public partial class LevelCreatorForm : Form
     {
+        private int _windowWidth = 0;
+        private List<Control> _objectsToResize = null!;
         private DataTable _table = new DataTable();
         private Panel _panel = new Panel();
-
-        private Rectangle _panel1OriginalRectangle;
-
-        private Rectangle _pictureBox1OriginalRectangle;
-        private Rectangle _pictureBox2OriginalRectangle;
-        private Rectangle _pictureBox3OriginalRectangle;
-        private Rectangle _pictureBox4OriginalRectangle;
-        private Rectangle _pictureBox5OriginalRectangle;
-        private Rectangle _pictureBox6OriginalRectangle;
-
-        private Rectangle _originalFormSize;
-
-        private Rectangle _label1OriginalRectangle;
-        private Rectangle _label2OriginalRectangle;
-        private Rectangle _label3OriginalRectangle;
-
-        private Rectangle _textBox1OriginalRectangle;
-        private Rectangle _textBox2OriginalRectangle;
-
-        private Rectangle _button1OriginalRectangle;
 
         public LevelCreatorForm()
         {
@@ -52,6 +32,12 @@ namespace CourseWork3rdHalfYear.Forms
 
         private void LevelCreatorForm_Load(object sender, EventArgs e)
         {
+            _windowWidth = this.Width;
+            _objectsToResize = new()
+            {
+                pictureBox5, pictureBox6, button1, label1, label2, label3, textBox1, textBox2
+            };
+
             for (int i = 0; i < 25; i++)
             {
                 PictureBox picBox;
@@ -62,90 +48,26 @@ namespace CourseWork3rdHalfYear.Forms
                 picBox.Load(@"D:\university\CourseWork3rdHalfYear\CourseWork3rdHalfYear\Resources\Box.png");
                 flowLayoutPanel1.Controls.Add(picBox);
             }
-            //_panel1OriginalRectangle = new Rectangle(panel1.Location.X, panel1.Location.Y, panel1.Width, panel1.Height);
 
-            //_pictureBox1OriginalRectangle = new Rectangle(pictureBox1.Location.X, pictureBox1.Location.Y, pictureBox1.Width, pictureBox1.Height);
-            //_pictureBox2OriginalRectangle = new Rectangle(pictureBox2.Location.X, pictureBox2.Location.Y, pictureBox2.Width, pictureBox2.Height);
-            //_pictureBox3OriginalRectangle = new Rectangle(pictureBox3.Location.X, pictureBox3.Location.Y, pictureBox3.Width, pictureBox3.Height);
-            //_pictureBox4OriginalRectangle = new Rectangle(pictureBox4.Location.X, pictureBox4.Location.Y, pictureBox4.Width, pictureBox4.Height);
-            //_pictureBox5OriginalRectangle = new Rectangle(pictureBox5.Location.X, pictureBox5.Location.Y, pictureBox5.Width, pictureBox5.Height);
-            //_pictureBox6OriginalRectangle = new Rectangle(pictureBox6.Location.X, pictureBox6.Location.Y, pictureBox6.Width, pictureBox6.Height);
-
-            //_label1OriginalRectangle = new Rectangle(label1.Location.X, label1.Location.Y, label1.Width, label1.Height);
-            //_label2OriginalRectangle = new Rectangle(label2.Location.X, label2.Location.Y, label2.Width, label2.Height);
-            //_label3OriginalRectangle = new Rectangle(label3.Location.X, label3.Location.Y, label3.Width, label3.Height);
-
-            //_textBox1OriginalRectangle = new Rectangle(textBox1.Location.X, textBox1.Location.Y, textBox1.Width, textBox1.Height);
-            //_textBox2OriginalRectangle = new Rectangle(textBox2.Location.X, textBox2.Location.Y, textBox2.Width, textBox2.Height);
-
-            //_button1OriginalRectangle = new Rectangle(button1.Location.X, button1.Location.Y, button1.Width, button1.Height);
-
-            //_originalFormSize = new Rectangle(this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height);
+            if (flowLayoutPanel1.Controls is not null)
+            {
+                foreach(var control in flowLayoutPanel1.Controls)
+                {
+                    if (control is PictureBox)
+                    {
+                        PictureBox pb = (control as PictureBox)!;
+                        pb.Size = new Size((flowLayoutPanel1.Width - 45) / 2, (flowLayoutPanel1.Height - 45) / 2);
+                    }
+                }
+            }
         }
-        private void resizeControl(Rectangle rectangle, Control control)
-        {
-            float xRatio = (float)(this.Width) / (float)(_originalFormSize.Width);
-            float yRatio = (float)(this.Height) / (float)(_originalFormSize.Height);
-
-            int newX = (int)(rectangle.X * xRatio);
-            int newY = (int)(rectangle.Y * yRatio);
-
-            int newWidth = (int)((rectangle.Width) * xRatio);
-            int newHeight = (int)((rectangle.Height) * yRatio);
-
-            control.Location = new Point(newX, newY);
-            control.Size = new Size(newWidth, newHeight);
-        }
-
-        private void resizeControl1(Rectangle rectangle, Control control)
-        {
-            float xRatio = (float)(this.Width) / (float)(_originalFormSize.Width);
-            float yRatio = (float)(this.Height) / (float)(_originalFormSize.Height);
-
-            int newX = (int)(rectangle.X * xRatio);
-            int newY = (int)(rectangle.Y * yRatio);
-
-            int newWidth = (int)((rectangle.Width) * xRatio);
-            int newHeight = (int)((rectangle.Height) * yRatio);
-
-            control.Location = new Point(newX, newY);
-            control.Size = new Size(newWidth, newHeight);
-        }
-
+        
         private void LevelCreatorForm_Resize(object sender, EventArgs e)
         {
-            pictureBox5.Location = new Point(889 + this.Width - 960, 4);
-            pictureBox6.Location = new Point(820 + this.Width - 960, 4);
+            foreach (Control control in _objectsToResize)
+                control.Location = new Point(control.Location.X + this.Width - _windowWidth, control.Location.Y);
 
-            button1.Location = new Point(720 + this.Width - 960, 8);
-
-            label1.Location = new Point(441 + this.Width - 960, 20);
-            label2.Location = new Point(666 + this.Width - 960, 0);
-            label3.Location = new Point(588 + this.Width - 960, 0);
-
-            textBox1.Location = new Point(603 + this.Width - 960, 20);
-            textBox2.Location = new Point(671 + this.Width - 960, 20);
-            /*resizeControl(_pictureBox1OriginalRectangle, pictureBox1);
-            resizeControl(_pictureBox2OriginalRectangle, pictureBox2);
-            resizeControl(_pictureBox3OriginalRectangle, pictureBox3);
-            resizeControl(_pictureBox4OriginalRectangle, pictureBox4);
-            resizeControl(_pictureBox5OriginalRectangle, pictureBox5);
-            resizeControl(_pictureBox6OriginalRectangle, pictureBox6);
-
-            resizeControl(_label1OriginalRectangle, label1);
-            resizeControl(_label2OriginalRectangle, label2);
-            resizeControl(_label3OriginalRectangle, label3);
-
-            resizeControl1(_dataGridView1OriginalRectangle, dataGridView1);
-
-            resizeControl(_panel1OriginalRectangle, panel1);*/
-            //resizeControl(_panel2OriginalRectangle, panel2);
-            /*resizeControl(_panel3OriginalRectangle, panel3);
-
-            resizeControl(_textBox1OriginalRectangle, textBox1);
-            resizeControl(_textBox2OriginalRectangle, textBox2);
-
-            resizeControl(_button1OriginalRectangle, button1);*/
+            _windowWidth = this.Width;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
--- CourseWork3rdHalfYear/obj/Debug/net8.0-windows/CourseWork3rdHalfYear.AssemblyInfo.cs
+++ CourseWork3rdHalfYear/obj/Debug/net8.0-windows/CourseWork3rdHalfYear.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork3rdHalfYear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40535c6844022d5bd318602fd6665fe7a344c4f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f306f692490fded668db1aab09408de28285d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork3rdHalfYear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork3rdHalfYear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
